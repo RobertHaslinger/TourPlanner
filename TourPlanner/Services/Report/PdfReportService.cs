20f4feaf23441b073fc2e9a69ca177680d13a26a
Robert Haslinger <robert.haslinger1999@gmail.com>
2021-05-28 12:38:35 +0200

    basic service

---
+++ TourPlanner/Services/Report/PdfReportService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TourPlanner.Models;
+
+namespace TourPlanner.Services.Report
+{
+    public class PdfReportService : IReportService
+    {
+        public void GenerateReport(Tour tour)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GenerateSummaryReport(List<Tour> tours)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
